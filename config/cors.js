1575377f48748e411f16cf9ba29b0fe0c6d72c59
muganzacoders-eng <muganzacoders@gmail.com>
2025-10-02 14:48:30 +0300

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,10 +1,13 @@
 const corsOptions = {
   origin: [
+    'https://www.relysi.com',
+    'https://uprelysi.vercel.app',
     'http://localhost:3000',
     'https://your-production-domain.com'
   ],
   credentials: true,
   optionsSuccessStatus: 200
 };
+
 
 module.exports = corsOptions;
